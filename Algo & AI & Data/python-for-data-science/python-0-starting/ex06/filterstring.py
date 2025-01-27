439c9c2db96df8867dde99c4e970abda1d912bf2
davidmonteiro03 <damonteiro12446@gmail.com>
2025-01-27 08:36:52 +0000

    git updates

--- Algo & AI & Data/python-for-data-science/python-0-starting/ex06/filterstring.py
+++ Algo & AI & Data/python-for-data-science/python-0-starting/ex06/filterstring.py
@@ -1,7 +1,11 @@
 from ft_filter import ft_filter
 import sys as s
 
+
 def main():
+    """This is a program that accepts two arguments: \
+a string(S), and an integer(N). The program returns a \
+list of words from S that have a length greater than N."""
     argc = len(s.argv)
     try:
         assert argc == 3, "the arguments are bad"
