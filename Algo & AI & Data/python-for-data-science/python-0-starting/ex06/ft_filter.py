1a94379fa887912f0643a921b4769f526302f03e
davidmonteiro03 <damonteiro12446@gmail.com>
2025-02-06 09:56:21 +0000

    git updates

--- Algo & AI & Data/python-for-data-science/python-0-starting/ex06/ft_filter.py
+++
@@ -1,4 +0,0 @@
-def ft_filter(function: any, iterable: list) -> list:
-    """This is my own ft_filter, which behave like \
-the original built-in filter function."""
-    return [elem for elem in iterable if function(elem)]
--- Algo & AI & Data/python-for-data-science/python-0-starting/ex09/ft_package/utils.py
+++
@@ -1,4 +0,0 @@
-def count_in_list(lst: list, elem) -> int:
-    """This function is used to count \
-the number of times an element is in a list."""
-    return lst.count(elem)
